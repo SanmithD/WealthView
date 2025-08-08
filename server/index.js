11ad8c22ff7144ffe7783c760fb77d3c94521386
SanmithD <sanmithdevadiga7@gmail.com>
2025-08-08 15:11:39 +0530

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-    origin: process.env.NODE_ENV !== 'development' ? process.env.CLIENT_URL : 'http://localhost:5173',
+    // origin: process.env.NODE_ENV !== 'development' ? process.env.CLIENT_URL : 'http://localhost:5173',
+    origin: 'https://wealth-view-seven.vercel.app/',
     credentials: true,
     methods: ['GET']
 }));
